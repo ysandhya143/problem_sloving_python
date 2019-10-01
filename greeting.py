32c2ab235df48097e94975eec31b753d02e2e957
sandya <yenumulasandhya1998@gmail.com>
2019-09-30 19:59:09 -0700

    msg

---
+++ greeting.py
@@ -0,0 +1,5 @@
+def hello(name):
+    return 'hello '+name
+
+def birthday(name):
+    return "happy birthday "+name
---
+++ package_art/basics_art.py
@@ -0,0 +1,6 @@
+def swap(a,b):
+    c=a
+    a=b
+    b=c
+    return a,b
+swap(2,3)
